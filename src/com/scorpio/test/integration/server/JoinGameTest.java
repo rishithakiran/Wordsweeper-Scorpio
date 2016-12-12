195143bf47b231c850e39cf25b4aa816f9f7032b
Josh Hebert <jhebes7@gmail.com>
2016-12-12 14:09:51 -0500

    More test %

--- src/com/scorpio/test/integration/server/JoinGameTest.java
+++ src/com/scorpio/test/integration/server/JoinGameTest.java
@@ -2,6 +2,7 @@ package com.scorpio.test.integration.server;
 
 import com.scorpio.server.controller.ConnectionController;
 import com.scorpio.server.controller.CreateGameRequestController;
+import com.scorpio.server.controller.LockGameRequestController;
 import com.scorpio.server.core.GameManager;
 import com.scorpio.server.exception.WordSweeperException;
 import com.scorpio.server.model.Game;
@@ -43,5 +44,39 @@ public class JoinGameTest {
 
         assert(playersBefore + 1 == playersAfter);
         assert(g.getPlayers().get(1).getName().equals("nextOne"));
+    }
+
+    /**
+     * Ensure that we get an error when failing to join a game because it is locked
+     */
+    @Test
+    public void functionality_HandleLocked(){
+        ConnectionController router = new ConnectionController();
+        CreateGameRequestController cgr = new CreateGameRequestController();
+        LockGameRequestController lgr = new LockGameRequestController();
+        try {
+            cgr.createGame(new Player("testPlayer", new FakeClientState("a")), "somePlace","abc");
+            lgr.lockGame("somePlace");
+
+        }catch(WordSweeperException ex){
+            fail();
+        }
+
+        Game g = GameManager.getInstance().findGameById("somePlace");
+        int playersBefore = g.getPlayers().size();
+        Message msg = xml.createMessageFromFile("testxml/joinGameRequest.xml");
+        String res = router.process(new FakeClientState("b"), msg).toString();
+        int playersAfter = g.getPlayers().size();
+
+        assert(playersBefore == playersAfter);
+        assert(g.getPlayers().get(0).getName().equals("testPlayer"));
+
+        String correctRes = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                "<response id=\"b\" reason=\"Game is locked!\" success=\"false\" version=\"1.0\">" +
+                "<joinGameResponse gameId=\"somePlace\"/>" +
+                "</response>";
+
+        assert(res.equals(correctRes));
+
     }
 }
